781eaa8e5471711ee9db04d270eb929588a739e0
acharyaapurvau <acharyaapurvau@gmail.com>
2025-10-28 12:48:58 +0530

    Move hangman.py into hangman folder

---
+++ hangman/hangman.py
@@ -0,0 +1,113 @@
+import random
+
+lives = 6
+stages = [
+    '''
+        +-------+
+        |
+        |
+        | 
+        |
+        |
+     ==============
+     ''',
+    '''
+        +-------+
+        |       |
+        |       0
+        | 
+        |
+        |
+     ==============
+     ''',
+    '''
+        +-------+
+        |       |
+        |       0
+        |       |
+        |
+        |
+     ==============
+     ''',
+    '''
+        +-------+
+        |       |
+        |       0
+        |      -|
+        |
+        |
+     ==============
+     ''',
+    '''
+        +-------+
+        |       |
+        |       0
+        |      -|-
+        |
+        |
+     ==============
+     ''',
+    '''
+        +-------+
+        |       |
+        |       0
+        |      -|-
+        |      /
+        |
+     ==============
+     ''',
+    '''
+        +-------+
+        |       |
+        |       0
+        |      -|-
+        |      / \\
+        |
+     ==============
+     '''
+]
+
+words_list = ["cheetah", "dolphin", "baboon"]
+word = random.choice(words_list)
+print("Welcome to Hangman!")
+placeholder = "_" * len(word)
+print("Word to guess: " + " ".join(placeholder))
+
+game_over = False
+correct_letters = []
+guessed_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in guessed_letters:
+        print(f"You already guessed '{guess}'. Try a different letter.\n")
+        continue
+
+    guessed_letters.append(guess)
+    display = ""
+
+    for letter in word:
+        if letter == guess or letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    # Add guessed letter to correct_letters if correct
+    if guess in word and guess not in correct_letters:
+        correct_letters.append(guess)
+
+    print("\n" + " ".join(display))
+    print(f"Guessed letters: {', '.join(guessed_letters)}")
+
+    if guess not in word:
+        lives -= 1
+        print(stages[6-lives])
+        print(f"You have {lives} lives left. ")
+        if lives == 0:
+            game_over = True
+            print(f"You lose! The word was '{word}'.")
+
+    if "_" not in display:
+        game_over = True
+        print("Congratulations! You win!")
